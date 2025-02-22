69dff3051d35788930b7e0f3a63892b08f093838
arsenii555 <arsenij.evdokimov@bk.ru>
2025-02-22 22:10:17 +0300

    add tree info

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -1,7 +1,6 @@
 from sys import argv
 import zlib
-from glob import iglob
-from os.path import basename, dirname
+from os.path import basename
 from pathlib import Path
 
 
@@ -13,6 +12,52 @@ def get_branches(repo_path):
 def get_last_commit_hash(branch_path):
     with open(branch_path) as f:
         return f.read().strip()
+
+
+def get_tree_hash(blob):
+    for line in blob.splitlines():
+        if line.startswith("tree"):
+            return line.split()[1]
+
+
+def parse_tree(tree_data):
+    _, _, content = tree_data.partition(b"\x00")
+    files = []
+    while content:
+        mode_name, content = content.split(b"\x00", 1)
+        mode, name = mode_name.split(b" ", 1)
+        sha1, content = content[:20], content[20:]
+        sha1_hex = sha1.hex()
+        obj_type = "blob" if mode.startswith(b"10") else "tree"
+        files.append(f"{obj_type} {sha1_hex} {name.decode()}")
+    return files
+
+
+def get_object(repo_path, obj_hash):
+    obj_path = repo_path / f"objects/{obj_hash[:2]}/{obj_hash[2:]}"
+    if obj_path.exists():
+        return zlib.decompress(obj_path.read_bytes())
+    return None
+
+
+def show_commit_history(repo_path, commit_hash):
+    curr_commit = get_object(repo_path, commit_hash)
+    if not curr_commit:
+        return
+
+    head, _, blob = curr_commit.partition(b"\x00")
+    blob = blob.decode()
+    tree = get_tree_hash(blob)
+    if tree:
+        tree_obj = get_object(repo_path, tree)
+        if tree_obj:
+            print(f"TREE for commit {commit_hash}")
+            print("\n".join(parse_tree(tree_obj)))
+
+    for line in blob.splitlines():
+        if line.startswith("parent"):
+            show_commit_history(repo_path, line.split()[1])
+            break
 
 
 match len(argv):
@@ -29,7 +74,9 @@ match len(argv):
             print("Ветка не найдена")
             exit()
         last_commit = get_last_commit_hash(repo_path / "refs/heads" / branch)
-        commit_object = zlib.decompress((repo_path / f"objects/{last_commit[:2]}/{last_commit[2:]}").read_bytes())
+        commit_object = get_object(repo_path, last_commit)
         head, _, blob = commit_object.partition(b"\x00")
         blob = blob.decode()
         print(blob)
+
+        show_commit_history(repo_path, last_commit)
